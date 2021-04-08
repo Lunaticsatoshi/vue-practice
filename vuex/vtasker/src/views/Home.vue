1a858b2618d6389e6fb8ce6b3fb61c08e7767c97
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-08 16:55:21 +0530

    :zap: Implemented User table in Home page

--- vuex/vtasker/src/views/Home.vue
+++ vuex/vtasker/src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home container mt-4">
     <h1 class="page-header">Manage Users</h1>
+    <table class="table table-striped mt-4">
+      <thead>
+        <tr>
+          <th>User ID</th>
+          <th>Username</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
   </div>
 </template>
 
