ea2b47837648e891850dd0a7df1b51a4f48f58cf
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-09 19:11:50 +0530

    :zap: Updating Alert component

--- vuex/vtasker/src/components/Alert.vue
+++ vuex/vtasker/src/components/Alert.vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="alert alert-warning alert-dismissible" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    {{message}}
-</div>
+  <div class="alert alert-warning alert-dismissible fade show mt-4" role="alert">
+    <strong>Error!</strong> {{ message }}
+    <button
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="alert"
+      aria-label="Close"
+    ></button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'alert',
-  props: ['message'],
-  data () {
-    return {
-      
-    }
-  }
-}
+  name: "alert",
+  props: ["message"],
+  data() {
+    return {};
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
