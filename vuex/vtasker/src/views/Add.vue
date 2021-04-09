c66abc649554d8c913b0a046669e22243f55de3d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-09 18:47:32 +0530

    :zap: Add User Component

--- vuex/vtasker/src/views/Add.vue
+++ vuex/vtasker/src/views/Add.vue
@@ -1,13 +1,46 @@
 <template>
-  
+  <div class="add container">
+    <Alert v-if="alert" v-bind:message="alert" />
+    <h1 class="page-header">Add Users</h1>
+    <form v-on:submit="addCustomer">
+      <div class="well">
+        <h4>Customer Info</h4>
+        <div class="form-group">
+          <label>Email</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Email"
+            v-model="email"
+          />
+        </div>
+        <div class="form-group">
+          <label>Username</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            v-model="username"
+          />
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary mt-4">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Add',
-}
+  name: "Add",
+  data () {
+      return {
+          email: '',
+          username: '',
+          alert: ''
+      }
+  }
+};
 </script>
 
 <style>
-
 </style>
