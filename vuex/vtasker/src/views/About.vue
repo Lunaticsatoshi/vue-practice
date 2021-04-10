1aa853dcfdf5ff60fd035d604c635b6aa20865c8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-10 21:07:16 +0530

    :zap: Created Vtasker: a task tracking app

--- vuex/vtasker/src/views/About.vue
+++ vuex/vtasker/src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about container">
-    <h1 class="page-header">About Vtasker</h1>
+    <div class="card mt-4" style="width: 18rem">
+      <!-- <img
+        src="https://avatars.githubusercontent.com/u/47326452?v=4"
+        class="card-img-top"
+        alt="..."
+      /> -->
+      <div class="card-body">
+        <h5 class="card-title">About Vtasker</h5>
+        <p class="card-text">
+          Vtasker is a individual task Tracking App made using Vue js for
+          tracking all the Todos specific users. Add new User and new tasks as
+          per the user.
+        </p>
+        <a href="https://github.com/Lunaticsatoshi" class="btn btn-primary"
+          >Github</a
+        >
+      </div>
+    </div>
   </div>
 </template>
