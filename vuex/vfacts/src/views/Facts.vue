7cd3ffe92668c81b8fdd255fda27dfe07eb39f98
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-20 17:43:28 +0530

    :zap: getting allFacts list in facts view

--- vuex/vfacts/src/views/Facts.vue
+++ vuex/vfacts/src/views/Facts.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
     <h1 class="text__facts"> All Facts Till Now</h1>
-    <h4>Facts</h4>
+    <h4 v-for="(fact,id) in allFacts" :key="id">
+      <span>{{id+1}}</span>
+      <span>{{fact}}</span>
+    </h4>
   </div>
 </template>
 
 <script>
-export default {};
+import {mapGetters} from "vuex"
+export default {
+  name: "Facts",
+  computed: {
+    ...mapGetters({allFacts: "getAllFacts"})
+  }
+};
 </script>
 
 <style scoped>
 h4 {
   color: blueviolet;
   font-weight: bolder;
-  font-size: 2.5rem;
+  font-size: 1.5rem;
 }
 
 .text__facts {
