d347d07ecc24c2169726b6c8960e03028d88a15a
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-20 16:33:50 +0530

    :zap: Designed Facts Component

--- vuex/vfacts/src/views/Facts.vue
+++ vuex/vfacts/src/views/Facts.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1 class="text__facts"> All Facts Till Now</h1>
+    <h4>Facts</h4>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h4 {
+  color: blueviolet;
+  font-weight: bolder;
+  font-size: 2.5rem;
+}
+
+.text__facts {
+  padding: 12px;
+  font-weight: bold;
+  font-size: 1.5rem;
+}
+</style>
