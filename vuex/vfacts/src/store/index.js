95aed00a0ffecb516c30b29fc1ccc5e696158bd2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-04-20 16:52:20 +0530

    :sparkles: setting up vuex

--- vuex/vfacts/src/store/index.js
+++ vuex/vfacts/src/store/index.js
@@ -2,10 +2,26 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    currentFact: '',
+    allFacts: []
+  },
+  getters: {
+    getCurrentFact: (state) => state.currentFact,
+    getAllFacts: (state) => state.allFacts
   },
   mutations: {
+    setCurrentFact(state,payload){
+      state.currentFact = payload;
+      state.allFacts.push(payload)
+    }
   },
   actions: {
+    async setCurrentFact({commit}){
+      const factResponse = await fetch('https://uselessfacts.jsph.pl/random.json')
+      const fact = factResponse.json();
+      commit("setCurrentFact", fact.text)
+
+    }
   },
   modules: {
   }
